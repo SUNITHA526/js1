717376ca465d01d73c7df9cb617741e71fa2552f
sunitha <gsita3251@gmail.com>
2024-11-30 11:03:38 +0530

    two files created

---
+++ js.js
@@ -0,0 +1,43 @@
+var age=prompt("enter your age here");
+if(age>=18){
+    console.log("you are eleigible for voting");
+}else{
+    console.log("you are not eligible to vote");
+}
+
+var male=prompt("enter your age here if you are male");
+if(male>=21){
+    console.log("you are eligible to get marry");
+    }else{
+    console.log("you are not eligible to get marry");
+}
+
+var female=prompt("enter your age here if you are female");
+if( female>=18){
+    console.log("you are eligible to get marry");
+}else{
+    console.log("you are not eligible to get marry");
+}
+
+var years=prompt("enter your age here");
+if(years>=18){
+    console.log("you are eligiblr to get licence");
+}else{
+    console.log("you are not eligible to get licence");
+}
+
+var attendance=prompt("enter your attendance percentage here");
+if(attendance>=75){
+    console.log("you are applicable to write an exam");
+}else{
+    console.log("you are not eligible to attend an exam");
+}
+
+var years=prompt("enter your age here");
+if(years>=60){
+    console.log("you are eligible to get discount for senior citize");
+}else{
+    console.log(" sorry you are not eligible to get discount")
+}
+
+
